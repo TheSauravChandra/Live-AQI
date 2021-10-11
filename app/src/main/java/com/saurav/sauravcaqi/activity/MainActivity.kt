13dfdefa74b8cffe9249a9f5d94371aa25c64a87
Saurav <saurav.chandra@wheelseye.com>
2021-10-11 12:45:27 +0530

    fix

--- app/src/main/java/com/saurav/sauravcaqi/activity/MainActivity.kt
+++ app/src/main/java/com/saurav/sauravcaqi/activity/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.transition.Fade
 import android.transition.Transition
 import android.transition.TransitionManager
-import androidx.appcompat.app.AlertDialog
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
