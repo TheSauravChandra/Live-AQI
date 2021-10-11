1b8760c04258f01e1ccd44594b4f53712376d750
Saurav <saurav.chandra@wheelseye.com>
2021-10-11 10:56:16 +0530

    consts

--- app/src/main/java/com/saurav/sauravcaqi/utils/Constants.kt
+++ app/src/main/java/com/saurav/sauravcaqi/utils/Constants.kt
@@ -5,4 +5,13 @@ object Constants {
   
   val MAX_TIME_SERIES = 15 // items. (~ 2sec)
   val MAX_GRADIENT_SERIES = 5 // items. (~ 2sec)
+  
+  ////////////////////////////////////////////////////////////
+  
+  const val AUTHOR = "SAURAV CHANDRA"
+  const val WEBSITE = "HTTP://SAURAVC.DX.AM/"
+  const val EMAIL = "sauravchandra94@gmail.com"
+  const val GITHUB = "https://github.com/TheSauravChandra"
+  
+  const val THANKS = "creator, parents & guru"
 }
